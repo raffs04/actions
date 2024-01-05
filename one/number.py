cc071c502ae665e4eee03c3c47fee252c41d4ae9
raffs04 <155355959+raffs04@users.noreply.github.com>
2024-01-06 00:08:14 +0530

    Update number.py

--- one/number.py
+++ one/number.py
@@ -1,21 +1,8 @@
-import random
+# app.py
+# This is a test commit
+def add(a, b):
+    return a + b
 
-
-def guessing_game():
-    number_to_guess = random.randint(1, 100)
-
-    while True:
-        guess = int(input("Guess the number (between 1 and 100): "))
-
-        if guess == number_to_guess:
-            print("Congratulations! You guessed the correct number.")
-            break
-        elif guess < number_to_guess:
-            print("Too low! Try again.")
-        else:
-            print("Too high! Try again.")
-
-
-if __name__ == "__main__":
-    print("Welcome to the Guessing Game!")
-    guessing_game()
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
