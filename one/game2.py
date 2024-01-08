bfea1ca56389ac040fa8d22d01ebede532d7391c
raffs04 <155355959+raffs04@users.noreply.github.com>
2024-01-08 14:12:30 +0530

    Update game2.py

--- one/game2.py
+++ one/game2.py
@@ -1,7 +1,6 @@
-#game2.py
 import random
 
-def guessing_game(get_input=input):
+def guessing_game(user_input_func=input):
     print("Welcome to the Guessing Game!")
 
     secret_number = random.randint(1, 100)
@@ -11,7 +10,7 @@ def guessing_game(get_input=input):
 
     while attempts < max_attempts:
         try:
-            guess = 42
+            guess = int(user_input_func("Guess the number (between 1 and 100): "))
             attempts += 1
 
             if guess == secret_number:
@@ -28,5 +27,16 @@ def guessing_game(get_input=input):
     else:
         print(f"Sorry, you've run out of attempts. The correct number was {secret_number}.")
 
-# Uncomment the next line if you want to run the game immediately when the file is executed
-guessing_game()
+# Call the function without input parameter to use standard input
+#guessing_game()
+
+def test_guessing_game():
+    # Example test where user always inputs 42
+    user_inputs = ["42"] * 10
+    user_input_iter = iter(user_inputs)
+    guessing_game(lambda _: next(user_input_iter))
+
+# Run the test
+test_guessing_game()
+
+
